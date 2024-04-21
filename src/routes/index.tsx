4b78b0d1cf6bbfc745bfb03cd1759cee648e02f5
PatrickJS <github@patrickjs.com>
2024-04-21 13:40:09 -0700

    feat: add cookie

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,20 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { $, component$, useSignal, useTask$ } from "@builder.io/qwik";
+import { server$, type DocumentHead } from "@builder.io/qwik-city";
+
+export const useServerData = server$(function () {
+  console.log("server data", this.cookie.get("__vdpl")!.value);
+  return {
+    name: "Qwik",
+  };
+});
 
 export default component$(() => {
+  const data = useSignal();
+  useTask$(async () => {
+    const res = await useServerData();
+    console.log("task", res);
+    data.value = res;
+  });
   return (
     <>
       <h1>Hi 👋</h1>
@@ -9,6 +22,17 @@ export default component$(() => {
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
+        <button
+          onClick$={[
+            $(async () => {
+              const res = await useServerData();
+              console.log("task", res);
+              data.value = res;
+            }),
+          ]}
+        >
+          Click me
+        </button>
       </p>
     </>
   );
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,15 +1,24 @@
 import { component$, Slot } from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-export const onGet: RequestHandler = async ({ cacheControl }) => {
+export const onGet: RequestHandler = async (resEv) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.dev/docs/caching/
-  cacheControl({
+  resEv.cacheControl({
     // Always serve a cached response by default, up to a week stale
     staleWhileRevalidate: 60 * 60 * 24 * 7,
     // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
     maxAge: 5,
   });
+};
+
+export const onRequest: RequestHandler = async (resEv) => {
+  if (process.env.VERCEL_SKEW_PROTECTION_ENABLED) {
+    resEv.headers.set(
+      "SET-COOKIE",
+      `__vdpl=${process.env.VERCEL_DEPLOYMENT_ID}; Path=${resEv.basePathname || "/"}; SameSite=Strict; Secure; HttpOnly`
+    );
+  }
 };
 
 export default component$(() => {
